64da8c4337f4bc9c083796441cf3bb02beff7f79
nocturnalpie <nicole@theliveseys.net>
2019-11-02 11:51:33 +0000

    setup

---
+++ CombiCon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CombiCon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

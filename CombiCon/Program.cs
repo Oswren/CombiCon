3d55317452ffa6bc02b2bb248056edeaae344f61
Joe Baguley <joe.baguley@outlook.com>
2019-11-02 12:51:18 +0000

    Initial setup

--- CombiCon/Program.cs
+++ CombiCon/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using JoyCon;
 
 namespace CombiCon
 {
     class Program
     {
+        private static List<Joycon> joycons;
+
+        // Values made available via Unity
+        public float[] stick;
+        public static Vector3 gyro;
+        public static Vector3 accel;
+        public static int jc_ind = 0;
+        public static Quaternion orientation;
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            JoyconManager jcMan = new JoyconManager();
+            jcMan.RefreshJoyConList();
+            Console.WriteLine(jcMan.JoyCons.ToString());
+
         }
     }
+    
 }

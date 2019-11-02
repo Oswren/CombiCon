f0df08538e2b910d3822d38e7e7d0fc706b64ce0
Joe Baguley <joe.baguley@outlook.com>
2019-11-02 13:37:44 +0000

    added support for joycons

--- CombiCon/Program.cs
+++ CombiCon/Program.cs
@@ -22,7 +22,26 @@ namespace CombiCon
             JoyconManager jcMan = new JoyconManager();
             jcMan.RefreshJoyConList();
             Console.WriteLine(jcMan.JoyCons.ToString());
-
+            Joycon j = jcMan.JoyCons[0];
+            Console.WriteLine(jcMan.JoyCons.Count);
+            DateTime oldtime = DateTime.Now;
+            DateTime newtime = DateTime.Now;
+            while (true)
+            {
+                newtime = DateTime.Now;
+                TimeSpan difference = newtime - oldtime;
+                j.Update(difference);
+                if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+                {
+                    Console.WriteLine("Shoulder pressed");
+                }
+                if (j.GetButtonDown(Joycon.Button.DPAD_LEFT))
+                {
+                    Console.WriteLine("DPAD Left pressed");
+                }
+                Console.WriteLine(string.Format("Gyro x: {0:N} Gyro y: {1:N} Gyro z: {2:N}", j.GetGyro().X, j.GetGyro().Y, j.GetGyro().Z));
+                oldtime = newtime;
+            }
         }
     }
     

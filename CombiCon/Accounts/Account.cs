9ff461c977789b97b3d9ee879b97b33835dfd487
Joe Baguley <joe.baguley@outlook.com>
2019-11-03 08:40:38 +0000

    fixde account

--- CombiCon/Accounts/Account.cs
+++ CombiCon/Accounts/Account.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
 namespace CombiCon.Accounts
 {
     public class Account
     {
-        public Account()
+        List<Vector3> _sequence;
+
+        public Account(List<Vector3> seq)
         {
+            _sequence = seq;
+        }
+
+        public double GenerateSimilarity(List<Vector3> attempt)
+        {
+            double similarity = 0;
+
+            for (int i = 0; i < _sequence.Count; i++)
+            {
+                Vector3 delta = attempt[i] - _sequence[i];
+                similarity += Math.Sqrt(Math.Pow(delta.X, 2) + Math.Pow(delta.Y, 2) + Math.Pow(delta.Z, 2));
+            }
+
+            return similarity;
         }
     }
 }

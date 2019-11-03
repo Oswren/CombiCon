e3088ea921d766415582268444707ed391da9565
Toby <toby.br314@gmail.com>
2019-11-03 10:06:38 +0000

    Added trained model and some computing power

--- CombiCon/Compute/shakeRecogniser.cs
+++ CombiCon/Compute/shakeRecogniser.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.ML;
+using CombiConML.Model;
 
 namespace CombiCon.Compute
 {
-    class shakeRecogniser
+    class ShakeRecogniser
     {
+        private const string DATA_FILEPATH = @"C:\Source\testDataset.csv";
+
+        // Method to load single row of dataset to try a single prediction
+        private IEnumerable<ModelInput> GetEnumerableSamples(string dataFilePath)
+        {
+            // Create MLContext
+            MLContext mlContext = new MLContext();
+
+            // Load dataset
+            IDataView dataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+                                            path: dataFilePath,
+                                            hasHeader: true,
+                                            separatorChar: ',',
+                                            allowQuoting: true,
+                                            allowSparse: false);
+
+            // Use first line of dataset as model input
+            // You can replace this with new test data (hardcoded or from end-user application)
+            IEnumerable<ModelInput> sampleForPrediction = mlContext.Data.CreateEnumerable<ModelInput>(dataView, false);
+            return sampleForPrediction;
+        }
     }
 }

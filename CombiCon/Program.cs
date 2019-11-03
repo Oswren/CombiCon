b01b6c0152e7b85d83b2225f2afaf52f808752e0
Joe Baguley <joe.baguley@outlook.com>
2019-11-03 00:45:03 +0000

    moved into passcode helper

--- CombiCon/Program.cs
+++ CombiCon/Program.cs
@@ -8,37 +8,15 @@ namespace CombiCon
 {
     class Program
     {
-        public static Vector3 _position;
-        private static JoyconHelper _helper = new JoyconHelper();
+        private static Vector3 _position;
+        private static PasscodeHelper _helper = new PasscodeHelper();
 
         static void Main(string[] args)
         {
+            List<Vector3> passcode1 = _helper.GenerateSequence();
+            List<Vector3> passcode2 = _helper.GenerateSequence();
 
-            Vector3 newTick;
-            List<Vector3> newSeq = new List<Vector3>();
 
-            while (true)
-            {
-                _position = _helper.PollJoyconForDirection();
-
-                newTick = new Vector3( (int)Math.Floor(_position.X * 4), (int)Math.Floor(_position.Y * 4), (int)Math.Floor(_position.Z * 4));
-
-                foreach (var item in newSeq)
-                {
-                    if (item.Equals(newTick))
-                    {
-                        _helper.VibrateJoycon(1.0f, 100);
-                    }
-                }
-
-                if (_helper.PollJoyconForButton(Joycon.Button.SHOULDER_2))
-                {
-                    Console.WriteLine(newTick.ToString());
-                    newSeq.Add(newTick);
-                }
-
-                Thread.Sleep(10);
-            }
         }
     }
-}
+}   

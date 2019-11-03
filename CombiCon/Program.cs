5dbabbb0ad195c231be5a09065b8e1eb1e4b614a
Joe Baguley <joe.baguley@outlook.com>
2019-11-03 09:10:26 +0000

    added users

--- CombiCon/Program.cs
+++ CombiCon/Program.cs
@@ -11,17 +11,47 @@ namespace CombiCon
     class Program
     {
         private static PasscodeHelper _helper = new PasscodeHelper();
-        private static List<Vector3> _savedPass;
-        private static List<Vector3> _passAttempt;
         private static MessageSender _messageSender;
+        private static Dictionary<String, Account> _accounts = new Dictionary<String, Account>();
 
         static void Main(string[] args)
         {
-
-            Account user = new Account(_helper.GenerateSequence());
+            CreateUser();
+            Account user = null;
+            do
+            {
+                Console.WriteLine("Please enter your username ");
+                String username = Console.ReadLine();
+                user = GetUser(username);
+                if (user == null)
+                {
+                    Console.WriteLine("Incorrect username!");
+                }
+            } while (user == null);
             Thread.Sleep(20);
             List<Vector3> passAttempt = _helper.GenerateSequence();
             CheckIfPasswordIsCorrect(user, passAttempt);
+        }
+
+        static void CreateUser()
+        {
+            Console.WriteLine("Please enter a username ");
+            String username = Console.ReadLine();
+            Account user = new Account(username, _helper.GenerateSequence());
+            _accounts.Add(username, user);
+            Console.WriteLine("User Created!");
+        }
+
+        static Account GetUser(String username)
+        {
+            try
+            {
+                return _accounts[username];
+            } catch (KeyNotFoundException e)
+            {
+                return null;
+            }
+            
         }
 
         private static void CheckIfPasswordIsCorrect(Account user, List<Vector3> passAttempt)
